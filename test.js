2ba4bffe0d8b906ba8cfa305cebc6716a4c54fbd
Luis Zuleta <luiszuletacm@gmail.com>
2024-12-26 17:32:39 -0500

    refactor: change logs for third line

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
 console.log("first line");
 console.log("second line");
-console.log("second line");
-console.log("second line");
-console.log("second line");
-console.log("second line");
-console.log("second line");
+console.log("third line");
